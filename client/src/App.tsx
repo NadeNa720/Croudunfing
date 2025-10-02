bc0dae7698918abf13c3082b218484d1e9f48be0
NadeNa720 <denis.jersivs@gmail.com>
2025-10-02 11:06:31 +0200

    Добавил страницу Realizacje и кнопку в шапке

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
-import Realizacje from "@/pages/Realizacje";
+
 
 function Router() {
   return (
