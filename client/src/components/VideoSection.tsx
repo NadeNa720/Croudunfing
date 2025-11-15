f61053108e4be1382503cad4e7fa65cf402cb34b
NadeNa720 <denis.jersivs@gmail.com>
2025-11-15 14:46:31 +0100

    video

--- client/src/components/VideoSection.tsx
+++ client/src/components/VideoSection.tsx
@@ -32,7 +32,7 @@ export default function VideoSection() {
             <CardContent className="p-0">
               <div className="relative aspect-video bg-muted">
                 <iframe
-                  src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fshare%2Fr%2F1HBC2qXMmc%2F&show_text=false&width=560&t=0"
+                  src="https://www.facebook.com/share/r/1JHvYPZz15/"
                   title="SprzątanieMieszkań.com na Facebooku"
                   className="absolute inset-0 w-full h-full"
                   allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
